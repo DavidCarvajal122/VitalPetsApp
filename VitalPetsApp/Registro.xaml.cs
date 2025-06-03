18b02d6859b0ba7c76482e8b6d8c2528d7011565
DavidCarvajal122 <david.carvajal.delrosario@udla.edu.ec>
2025-06-03 12:51:30 -0500

    Update a login y registro

--- VitalPetsApp/Registro.xaml.cs
+++ VitalPetsApp/Registro.xaml.cs
@@ -9,19 +9,25 @@ public partial class Registro : ContentPage
 
     private async void OnRegisterClicked(object sender, EventArgs e)
     {
+        if (string.IsNullOrWhiteSpace(newUserEntry.Text) ||
+            string.IsNullOrWhiteSpace(newPasswordEntry.Text) ||
+            string.IsNullOrWhiteSpace(confirmPasswordEntry.Text) ||
+            string.IsNullOrWhiteSpace(phoneEntry.Text) ||
+            string.IsNullOrWhiteSpace(emailEntry.Text))
+        {
+            await DisplayAlert("Error", "Por favor complete todos los campos", "OK");
+            return;
+        }
+
         if (newPasswordEntry.Text != confirmPasswordEntry.Text)
         {
             await DisplayAlert("Error", "Las contraseñas no coinciden", "OK");
             return;
         }
 
-        if (string.IsNullOrWhiteSpace(newUserEntry.Text) || string.IsNullOrWhiteSpace(newPasswordEntry.Text))
-        {
-            await DisplayAlert("Error", "Campos vacíos", "OK");
-            return;
-        }
-
         await DisplayAlert("Registro Exitoso", "Usuario creado correctamente", "OK");
-        await Navigation.PopAsync(); // Regresa al Login
+
+
+        await Navigation.PopAsync();
     }
 }
